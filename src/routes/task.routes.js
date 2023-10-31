3c62606260b695e325bf05db4552e3ca32ea35a3
kevin <acosta.kevin@correounivalle.edu.co>
2023-10-31 11:52:50 -0500

    cambios antes del pull

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -5,8 +5,12 @@ const router = express.Router();
 const { getallBooks} = require('../controllers/task.controllers.js');
 // Conectar a la base de datos al inicializar el router
 
+const authentication = require('../controllers/register.js');
+
 router.get('/api/books', getallBooks);
 
+
+router.post('/register', authentication.register);
 
 
 module.exports = router;
