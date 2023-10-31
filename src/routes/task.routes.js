440939f689bcc7f6a94a314479f9fcecb3ab0a28
kevin <acosta.kevin@correounivalle.edu.co>
2023-10-31 12:10:54 -0500

    modificacion

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -6,9 +6,11 @@ router.use(express.json());
 const { getallBooks} = require('../controllers/task.controllers.js');
 
 const authentication = require('../controllers/register.js');
+const loginrouter = require('../controllers/login.js');
 
 router.get('/api/books', getallBooks);
 
+router.post('/api/login', loginrouter);
 
 router.post('/register', authentication.register);
 
