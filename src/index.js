6d39c8988f527ed0e2d7a2a7cb1b52c2475b84fe
Jenifer Castaño Ledesma <jennifercasled@gmail.com>
2023-11-02 17:19:13 -0500

    Arreglo cors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 
 const router = require('./routes/task.routes');
 const loginrouter = require('./controllers/login');
 
 const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:5173' // solo permite recibir de esta funcion
+}));
+
 
 app.set('port', 4000 || process.env.PORT);
 
