93e6671a498bab93f866163932ce05a2096d5116
Santiago4271 <santiagogarciagil9@gmail.com>
2023-11-05 13:29:01 -0500

    hola a todos

--- src/db.js
+++ src/db.js
@@ -4,11 +4,10 @@ const pgp = require('pg-promise')();
 const db = pgp
 ({
   user: 'postgres',
-  user: 'postgres',
   host: 'localhost',
   database: 'postgres',
   password: 'r2301311',
-  port: 5432,
-})
+  port: 5432
+});
 
 module.exports = db;
