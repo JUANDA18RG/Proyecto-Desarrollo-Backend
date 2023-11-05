fccb7e35b48478af62c1abbbf863443461c6cd95
kevin <acosta.kevin@correounivalle.edu.co>
2023-11-05 00:08:04 -0500

    agregando cambios de validaciones

--- src/db.js
+++ src/db.js
@@ -6,8 +6,8 @@ const db = pgp
   user: 'postgres',
   host: 'localhost',
   database: 'BookFinder',
-  password: '12345',
-  port: 5432,
+  password: 'Acos306254',
+  port: 5432
 })
 
 module.exports = db;
