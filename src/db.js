45ca1d2febb0e37e2f575c3cf9b4d991af402a2e
Jenifer Castaño Ledesma <jennifercasled@gmail.com>
2023-11-11 16:46:08 -0500

    Base de datos en la nube

--- src/db.js
+++ src/db.js
@@ -1,13 +1,13 @@
-
 const pgp = require('pg-promise')();
 
 const db = pgp
 ({
-  user: 'postgres',
-  host: process.env.HOST,
-  database: process.env.DATABASE,
-  password: process.env.PASSWORD,
-  port: 5432
+  user: process.env.POSTGRES_USER,
+  host: process.env.POSTGRES_HOST,
+  database: process.env.POSTGRES_DATABASE,
+  password: process.env.POSTGRES_PASSWORD,
+  port: 5432,
+  ssl: { rejectUnauthorized: false }
 })
 
 module.exports = db;
