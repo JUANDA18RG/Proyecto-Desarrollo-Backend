dce2be88f7423600aca0729804ac6e9a95c3226a
Jenifer Castaño Ledesma <jennifercasled@gmail.com>
2023-11-18 16:06:41 -0500

    imagenes libros nuevos

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ const path = require('path');
 const pgp = require('pg-promise')();
 require('dotenv').config();
 
-const db = pgp({
+/*const db = pgp({
   user: process.env.POSTGRES_USER,
   host: process.env.POSTGRES_HOST,
   database: process.env.POSTGRES_DATABASE,
@@ -13,6 +13,17 @@ const db = pgp({
     ca: fs.readFileSync(path.join(__dirname, '..', 'certs', 'DigiCertGlobalRootCA.crt.pem')).toString()
   },
   port: 5432,
-});
+});*/
+
+const db = pgp({
+  user: 'postgres',
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
+  port: 5432,
+})
+
+
+
 module.exports = db;
 
