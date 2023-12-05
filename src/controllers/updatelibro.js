fdcdab6c2bdbbc9d9d5df6f01f9da18f8a756386
Niko <nicolas.arturo.valencia@correounivalle.edu.co>
2023-12-05 16:02:16 -0500

    andreitaaaaaaaaaaaaaaaaaaa

--- src/controllers/updatelibro.js
+++ src/controllers/updatelibro.js
@@ -1,3 +1,42 @@
+const db = require('../db.js');
+const fs = require('fs').promises;
+const path = require('path');
+
+// Ruta donde se almacenan las imágenes de portada (ajústala según tu estructura)
+const uploadPath = path.join(__dirname, '..', 'uploads');
+
+// Controlador para obtener todos los libros
+async function getAllLibros(req, res) {
+  try {
+    const libros = await db.any('SELECT * FROM libro');
+    res.json(libros);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error al obtener los libros' });
+  }
+}
+
+// Controlador para buscar un libro por Título (insensible a mayúsculas/minúsculas y tildes)
+async function getLibroByTitulo(req, res) {
+  const titulo = req.params.titulo;
+
+  try {
+    const libro = await db.oneOrNone(`
+      SELECT *
+      FROM libro
+      WHERE titulo ILIKE $1`, `%${titulo}%`);
+
+    if (!libro) {
+      return res.status(404).json({ error: 'Libro no encontrado' });
+    }
+
+    res.json(libro);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error al buscar el libro por Título' });
+  }
+}
+// Controlador para actualizar un libro por ISBN
 async function updateLibro(req, res) {
   const isbn = req.params.isbn;
   const { titulo, autor, genero, cantcopias, sinopsis, anioPublicacion } = req.body;
@@ -9,7 +48,7 @@ async function updateLibro(req, res) {
     }
 
     // Verifica si se proporciona una nueva portada en la solicitud
-    if (req.file) {
+    if (req.file && req.file.filename) {
       // Elimina la portada anterior si existe
       if (libroExistente.portada) {
         const portadaPath = path.join(uploadPath, libroExistente.portada);
@@ -22,7 +61,7 @@ async function updateLibro(req, res) {
     }
 
     // Calcula el nuevo valor para copiasdisponibles
-    const nuevasCopiasDisponibles = Math.max(0, libroExistente.cantreservas - cantcopias);
+    const nuevasCopiasDisponibles = Math.max(0, cantcopias - libroExistente.cantreservas);
 
     // Actualiza la información del libro en la base de datos
     const updateQuery = `
@@ -37,7 +76,10 @@ async function updateLibro(req, res) {
           copiasdisponibles = $9
       WHERE isbn = $1`;
 
-    await db.none(updateQuery, [isbn, titulo, autor, genero, cantcopias, sinopsis, anioPublicacion, req.file.filename, nuevasCopiasDisponibles]);
+    // Usa null si req.file.filename no está definido
+    const filename = req.file ? req.file.filename : null;
+
+    await db.none(updateQuery, [isbn, titulo, autor, genero, cantcopias, sinopsis, anioPublicacion, filename, nuevasCopiasDisponibles]);
 
     res.json({ status: 'ok', message: 'Libro actualizado exitosamente' });
   } catch (error) {
@@ -45,3 +87,11 @@ async function updateLibro(req, res) {
     res.status(500).json({ error: 'Error al actualizar el libro' });
   }
 }
+
+
+module.exports = {
+  getAllLibros,
+  getLibroByTitulo,
+  updateLibro,
+};
+
