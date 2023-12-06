417ba212d96eaeea614f704edd28aa7f0ee3f4e7
JUAN DAVID RAMIREZ GRISMALDO <105172937+JUANDA18RG@users.noreply.github.com>
2023-12-06 14:02:58 -0500

    version JuanDa

--- src/controllers/createBook.js
+++ src/controllers/createBook.js
@@ -47,8 +47,9 @@ const createbook = async (req, res) => {
             await deleteImage(req.file.path);
             return res.status(400).send({message: 'no hay copias para ingresar el libro a la biblioteca'});
         }
+        const portada = `/${req.file.filename}`;
 
-        await psql.createBook(ISBN, Titulo, Autor, Genero, anioPublicacion, Cantcopias, descripcion, req.file.filename);
+        await psql.createBook(ISBN, Titulo, Autor, Genero, anioPublicacion, Cantcopias, descripcion, portada);
 
         return res.status(201).send({message: `libro ${Titulo} creado exitosamente`});
 
